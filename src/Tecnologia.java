aad298be9efad78933da7574abdf0bb46d967124
OmarBackend <omar.backend4@gmail.com>
2024-05-02 10:39:35 -0500

    creamos constructores

--- src/Tecnologia.java
+++ src/Tecnologia.java
@@ -1,2 +1,7 @@
-public class Tecnologia {
+public class Tecnologia extends Industria{
+
+
+    public Tecnologia(Desarrollador[] desarrolladores, AnalistaFinanciero[] analistas, IngenieroProduccion[] ingenieros, ConsultorNegocios[] consultores) {
+        super(desarrolladores, analistas, ingenieros, consultores);
+    }
 }
