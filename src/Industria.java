a137e00dc2f36573358c3d083460270677dc48f9
davhx03 <daherreran@unal.edu.co>
2024-05-02 10:31:29 -0500

    Definición atributos Industria

--- src/Industria.java
+++ src/Industria.java
@@ -1,3 +1,13 @@
 public abstract class Industria {
+    private Desarrollador[] desarrolladores;
+    private AnalistaFinanciero[] analistas;
+    private IngenieroProduccion[] ingenieros;
+    private ConsultorNegocios[] consultores;
 
+    public Industria(Desarrollador[] desarrolladores, AnalistaFinanciero[] analistas, IngenieroProduccion[] ingenieros, ConsultorNegocios[] consultores) {
+        this.desarrolladores = desarrolladores;
+        this.analistas = analistas;
+        this.ingenieros = ingenieros;
+        this.consultores = consultores;
+    }
 }
